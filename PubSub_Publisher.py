d102462472b660b228ea0d0aaec483023f5e121d
Siilvaaz <48760038+Siilvaaz@users.noreply.github.com>
2024-02-22 11:31:42 +0000

    Update PubSub_Publisher.py

--- PubSub_Publisher.py
+++ PubSub_Publisher.py
@@ -1,27 +1,16 @@
 # PubSub_Publisher.py
 
-import sys
 from google.cloud import pubsub_v1
 
-def publish_message(project_id, topic_name, input_payload):
+def publish_message(project_id, topic_name, message):
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_name)
-
-    # Publishes a message
-    future = publisher.publish(topic_path, data=input_payload.encode("utf-8"))
+    future = publisher.publish(topic_path, data=message.encode('utf-8'))
     message_id = future.result()
+    print(f"Published message to {topic_path}. Message ID: {message_id}")
 
-    # Write message ID to file
-    with open('pubsub_ack.txt', 'w') as ack_file:
-        ack_file.write(str(message_id))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print(f"Usage: python {sys.argv[0]} project_id topic_name input_payload")
-        sys.exit(1)
-
-    project_id = sys.argv[1]
-    topic_name = sys.argv[2]
-    input_payload = sys.argv[3]
-
-    publish_message(project_id, topic_name, input_payload)
+if __name__ == '__main__':
+    project_id = 'sandbox-anthos'
+    topic_name = 'jenkins_poc'
+    message = 'your_message_payload'
+    publish_message(project_id, topic_name, message)
